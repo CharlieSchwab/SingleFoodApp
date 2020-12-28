ee8d7152b491b66b310c32a89f462c5ebfabfcdb
CharlieSchwab <chrlschwb@gmail.com>
2020-12-28 08:11:09 +0300

    app added

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,21 +2,11 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 import App from './App.vue';
-import VueRouter from 'vue-router';
-import VueAxios from 'vue-axios';
-import axios from 'axios';
-import {routes} from './routes';
-
-Vue.use(VueRouter);
-Vue.use(VueAxios, axios);
-
-const router = new VueRouter({
-    mode: 'history',
-    routes: routes
-});
 
 const app = new Vue({
     el: '#app',
-    router: router,
+    components : {
+        App
+    },
     render: h => h(App),
 });
