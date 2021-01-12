11ea6ed05d4dcde9535c9c8b132f3fac1974ca61
CharlieSchwab <chrlschwb@gmail.com>
2021-01-12 19:16:40 +0300

    stripe v3

--- app/Http/Controllers/StripeController.php
+++ app/Http/Controllers/StripeController.php
@@ -1,18 +1,15 @@
 <?php
 
-
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
 use Exception;
-use App\Payment;
+
 use Stripe\Charge;
 use Stripe\Stripe;
 use Stripe\Customer;
-
 
 
 class StripeController extends Controller
@@ -22,37 +19,6 @@ class StripeController extends Controller
         return view('client.stripe');
     }
   
-    /**
-     * handling payment with POST
-     */
-    public function handlePost2(Request $request)
-    {
-        
-        // $pur_arr = json_decode($request->purchase_list);
-        // $user_id = $request->userID;
-        // $restaurant_id = $request->restaurantID;
-
-        // echo $user_id;
-        // echo $restaurant_id;
-
-        // foreach ($pur_arr as $pur){
-        //     echo $pur->itemID;
-        // } 
-
-
-        Stripe::setApiKey(env('STRIPE_SECRET'));
-        Charge::create ([
-                "amount" => 100 * $request->chargeAmount,
-                "currency" => "USD",
-                "source" => $request->stripeToken,
-                "description" => "Making test payment." 
-        ]);
-  
-        Session::flash('success', 'Payment has been successfully processed.');
-          
-        return back();
-    }
-
     public function handlePost(Request $request)
     {
         request()->validate([
@@ -61,18 +27,19 @@ class StripeController extends Controller
             'terms_conditions' => 'accepted'
         ]);
 
-        /** I have hard coded amount. You may fetch the amount based on customers order or anything */
-        $amount     = 1 * 100;
+        $amount     = $request->tot_price * 100;
         $currency   = 'usd';
 
         if (empty(request()->get('stripeToken'))) {
             session()->flash('error', 'Some error while making the payment. Please try again');
             return back()->withInput();
         }
+
         
-        Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        Stripe::setApiKey(env('STRIPE_SECRET'));
         try {
-            /** Add customer to stripe, Stripe customer */
+
             $customer = Customer::create([
                 'email'     => request('email'),
                 'source'    => request('stripeToken')
@@ -81,10 +48,9 @@ class StripeController extends Controller
             $apiError = $e->getMessage();
         }
 
+
         if (empty($apiError) && $customer) {
-            /** Charge a credit or a debit card */
             try {
-                /** Stripe charge class */
                 $charge = Charge::create(array(
                     'customer'      => $customer->id,
                     'amount'        => $amount,
@@ -95,23 +61,12 @@ class StripeController extends Controller
                 $apiError = $e->getMessage();
             }
 
+
             if (empty($apiError) && $charge) {
-                // Retrieve charge details 
+
                 $paymentDetails = $charge->jsonSerialize();
                 if ($paymentDetails['amount_refunded'] == 0 && empty($paymentDetails['failure_code']) && $paymentDetails['paid'] == 1 && $paymentDetails['captured'] == 1) {
-                    /** You need to create model and other implementations */
-                    /*
-                    Payment::create([
-                        'name'                          => request('name'),
-                        'email'                         => request('email'),
-                        'amount'                        => $paymentDetails['amount'] / 100,
-                        'currency'                      => $paymentDetails['currency'],
-                        'transaction_id'                => $paymentDetails['balance_transaction'],
-                        'payment_status'                => $paymentDetails['status'],
-                        'receipt_url'                   => $paymentDetails['receipt_url'],
-                        'transaction_complete_details'  => json_encode($paymentDetails)
-                    ]);
-                    */
+             
                     return redirect('/thankyou/?receipt_url=' . $paymentDetails['receipt_url']);
                 } else {
                     session()->flash('error', 'Transaction failed');
--- resources/views/client/stripe.blade.php
+++ resources/views/client/stripe.blade.php
@@ -124,6 +124,10 @@
                     <input id="cash" type="radio" name="payment-method">
                     <label for="cash">Pay with Cash</label>
                 </div>
+                <input type="hidden" id="purchase_list" name="purchase_list">
+                <input type="hidden" id="restaurantID" name="restaurantID">
+                <input type="hidden" id="userID" name="userID">
+                <input type="hidden" id="tot_price" name="tot_price">
                 <div class="row form-group">
                     <div class="col-md-6 col-md-offset-3">
                         <input type="submit" value="Place Order" class="btn btn-primary pay-via-stripe-btn">
@@ -191,11 +195,9 @@
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
 <script src="https://js.stripe.com/v3/"></script>
 <script>
-    // Create an instance of the Stripe object
-    // Set your publishable API key
+
     var stripe = Stripe('{{ env("STRIPE_KEY") }}');
 
-    // Create an instance of elements
     var elements = stripe.elements();
 
     var style = {
@@ -230,7 +232,6 @@
     });
     cvc.mount('#card_cvc');
 
-    // Validate input of the card elements
     var resultContainer = document.getElementById('paymentResponse');
     cardElement.addEventListener('change', function (event) {
         if (event.error) {
@@ -240,38 +241,31 @@
         }
     });
 
-    // Get payment form element
     var form = document.getElementById('payment-form');
 
-    // Create a token when the form is submitted.
     form.addEventListener('submit', function (e) {
         e.preventDefault();
         createToken();
     });
 
-    // Create single-use token to charge the user
     function createToken() {
         stripe.createToken(cardElement).then(function (result) {
             if (result.error) {
-                // Inform the user if there was an error
+
                 resultContainer.innerHTML = '<p>' + result.error.message + '</p>';
             } else {
-                // Send the token to your server
                 stripeTokenHandler(result.token);
             }
         });
     }
 
-    // Callback to handle the response from stripe
     function stripeTokenHandler(token) {
-        // Insert the token ID into the form so it gets submitted to the server
         var hiddenInput = document.createElement('input');
         hiddenInput.setAttribute('type', 'hidden');
         hiddenInput.setAttribute('name', 'stripeToken');
         hiddenInput.setAttribute('value', token.id);
         form.appendChild(hiddenInput);
 
-        // Submit the form
         form.submit();
     }
 
